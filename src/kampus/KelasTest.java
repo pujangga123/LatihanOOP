7df6faa994e224c919968f615d8460b546da8059
pujangga123 <rush_x@yahoo.com>
2024-06-17 22:28:03 +0700

    add test kelas

--- src/kampus/KelasTest.java
+++ src/kampus/KelasTest.java
@@ -1,13 +1,29 @@
 package kampus;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class KelasTest {
 
     public static void main(String[] args) {
         Kelas kelas = new Kelas();
+        
+        //mempersiapkan data mahasiswa 
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.nim = "222";
+        mahasiswa.nama = "GEGES";
+        mahasiswa.tambah();
+        
+        //mempersiapkan data dosen
+        Dosen dosen = new Dosen();
+        dosen.nid = "2222";
+        dosen.nama = "GUNAWAN";
+        dosen.status = "AKTIF";
+        dosen.tambah();
 
         // test TAMBAH
-        kelas.kode = "A1";
-        kelas.dosenNid = "111";
+        kelas.kode = "A02";
+        kelas.dosenNid = "2222";
         kelas.status = "AKTIF";
         if (kelas.tambah()) {
             System.out.println("TAMBAH OK");
@@ -17,7 +33,7 @@ public class KelasTest {
         }
 
         // test BACA
-        kelas.baca("111");
+        kelas.baca("A02"); // 
         if (!kelas.kode.isEmpty()) {
             System.out.println("BACA OK");
         } else {
@@ -32,6 +48,19 @@ public class KelasTest {
         } else {
             System.out.println("UPDATE GAGAL");
             System.out.println(kelas.getErrMsg());
+        }
+        
+        // tambah peserta
+        if(kelas.pesertaTambah("222")) {
+            System.out.println("TAMBAH PESERTA OK");
+        } else {
+            System.out.println("TAMBAH PESERTA GAGAL: "+kelas.getErrMsg());
+        }
+        
+        System.out.println("MENAMPILKAN DAFTAR PESERTA KELAS A02");
+        List<Peserta> daftar = kelas.getListPeserta();
+        for(Peserta row: daftar) {
+            System.out.println(row.nim+" "+row.getMahasiswaNama()+" "+row.kehadiran+" "+row.tugas+" "+row.ujian);
         }
     }
 }
